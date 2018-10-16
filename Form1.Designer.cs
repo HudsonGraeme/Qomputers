a783645c838a701309eae2849d876834b46b86ec
Hudson Graeme <pwner@live.ca>
2018-10-16 14:38:52 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ICS3U1_Culminating
+{
+    partial class InitialView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MainLabel.Location = new System.Drawing.Point(290, 195);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(187, 40);
+            this.MainLabel.TabIndex = 0;
+            this.MainLabel.Text = "Qomputers";
+            // 
+            // InitialView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(866, 484);
+            this.Controls.Add(this.MainLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "InitialView";
+            this.Text = "Qomputers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MainLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICS3U1_Culminating
+{
+    public partial class InitialView : Form
+    {
+        public InitialView()
+        {
+            InitializeComponent();
+        }
+        Timer labelTransition = new Timer();
+        private void InitializeTimer()
+        {
+            labelTransition.Interval = 1000;
+            labelTransition.Enabled = true;
+            labelTransition.Tick += new EventHandler(labelTransition_Tick);
+            labelTransition.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PrivateFontCollection hind = new PrivateFontCollection();
+            hind.AddFontFile(Application.StartupPath + "\\resources\\" + "Hind-Light.ttf");
+            MainLabel.Font = new Font(hind.Families[0], 40);
+            InitializeTimer();
+        }
+
+        private TextBox UsernameEntry = new TextBox();
+        private void InitializeUsernameField()
+        {
+
+        }
+        private void labelTransition_Tick(object sender, EventArgs e)
+        {
+            if(UsernameEntry.Width == 0)
+            {
+                InitializeUsernameField();
+            }
+            if(MainLabel.Location.Y >= -10)
+            {
+                MainLabel.Location = new Point(MainLabel.Location.X, MainLabel.Location.Y - 1);
+                Console.WriteLine(MainLabel.Location);
+            } else
+            {
+                labelTransition.Stop();
+            }
+            if(labelTransition.Interval == 1000)
+            {
+                labelTransition.Interval = 10;
+            }
+        }
+
+
+
+    }
+}
